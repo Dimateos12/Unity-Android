44e169d19df9d9e03f586b5dc09b9965c90cef3c
Bartosz Jabłoński <bartekj1221@wp.pl>
2020-07-14 15:01:31 +0200

    14.07.2020

---
+++ Gra telefon - test/Assets/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Transform playerTransform;
+
+    private int currentIndexMove = 1;
+    private int xMove = 0;
+
+    void Start()
+    {
+        playerTransform = GetComponent<Transform>();
+    }
+
+    void Update()
+    {   
+
+        if (Input.GetKeyDown("left") && xMove > -2)
+        {
+            xMove -= 2;
+            playerTransform.position = new Vector2(xMove, playerTransform.position.y);
+        }
+
+        if (Input.GetKeyDown("right") && currentIndexMove < 2)
+        {
+            xMove += 2;
+            playerTransform.position = new Vector2(xMove, playerTransform.position.y);
+        }
+        
+    }
+}
